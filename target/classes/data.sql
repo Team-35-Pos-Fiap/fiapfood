681ce89da5cd56d4699e32926c3afb7ce7f8c81f
tmsouzarj <tmsouzarj@gmail.com>
2025-05-14 23:26:17 -0300

    ajustes nos métodos de atualização
    ajustes nos métodos de atualização

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,43 +1,43 @@
 insert into perfil (nome) values ('Dono');
 insert into perfil (nome) values ('Cliente');
 
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Thiago Motta', 'thiago@restaurantetudibaum.com', 1, current_timestamp, null, 1);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 2', 'usuario2@restaurantetudibaum.com', 1, current_timestamp, null, 2);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 3', 'usuario3@restaurantetudibaum.com', 0, current_timestamp, current_timestamp, 1);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 4', 'usuario4@restaurantetudibaum.com', 0, current_timestamp, current_timestamp, 2);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 5', 'usuario5@restaurantetudibaum.com', 1, current_timestamp, null, 1);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 6', 'usuario6@restaurantetudibaum.com', 1, current_timestamp, null, 1);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 7', 'usuario7@restaurantetudibaum.com', 0, current_timestamp, current_timestamp, 1);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 8', 'usuario8@restaurantetudibaum.com', 0, current_timestamp, current_timestamp, 2);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 9', 'usuario9@restaurantetudibaum.com', 1, current_timestamp, null, 2);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 10', 'usuario10@restaurantetudibaum.com', 1, current_timestamp, null, 2);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 11', 'usuario11@restaurantetudibaum.com', 0, current_timestamp, current_timestamp, 2);
-insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 12', 'usuario12@restaurantetudibaum.com', 0, current_timestamp, current_timestamp, 2);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Thiago Motta', 'thiago@fiapfood.com', 1, current_timestamp, null, 1);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 2', 'usuario2@fiapfood.com', 1, current_timestamp, null, 2);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 3', 'usuario3@fiapfood.com', 0, current_timestamp, current_timestamp, 1);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 4', 'usuario4@fiapfood.com', 0, current_timestamp, current_timestamp, 2);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 5', 'usuario5@fiapfood.com', 1, current_timestamp, null, 1);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 6', 'usuario6@fiapfood.com', 1, current_timestamp, null, 1);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 7', 'usuario7@fiapfood.com', 0, current_timestamp, current_timestamp, 1);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 8', 'usuario8@fiapfood.com', 0, current_timestamp, current_timestamp, 2);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 9', 'usuario9@fiapfood.com', 1, current_timestamp, null, 2);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 10', 'usuario10@fiapfood.com', 1, current_timestamp, null, 2);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 11', 'usuario11@fiapfood.com', 0, current_timestamp, current_timestamp, 2);
+insert into usuario (nome, email, ativo, data_criacao, data_atualizacao, id_perfil) values ('Usuario 12', 'usuario12@fiapfood.com', 0, current_timestamp, current_timestamp, 2);
 
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (1, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', '79', 'Casa 8', 1, '24455450');
+values (1, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', 1, 'Complemento do endereço', 1, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (2, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', null, null, 0, '24455450');
+values (2, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', null, null, 0, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (3, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', '79', 'Casa 8', 1, '24455450');
+values (3, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', 1, 'Complemento do endereço', 1, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (4, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', null, null, 0, '24455450');
+values (4, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', null, null, 0, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (5, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', '79', 'Casa 8', 1, '24455450');
+values (5, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', 1, 'Complemento do endereço', 1, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (6, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', null, null, 0, '24455450');
+values (6, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', null, null, 0, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (7, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', '79', 'Casa 8', 1, '24455450');
+values (7, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', 1, 'Complemento do endereço', 1, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (8, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', null, null, 0, '24455450');
+values (8, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', null, null, 0, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (9, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', '79', 'Casa 8', 1, '24455450');
+values (9, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', 1, 'Complemento do endereço', 1, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (10, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', null, null, 0, '24455450');
+values (10, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', null, null, 0, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (11, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', '79', 'Casa 8', 1, '24455450');
+values (11, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', 1, 'Complemento do endereço', 1, '24455000');
 insert into endereco (id_usuario, cidade, bairro, estado, endereco, numero, complemento, sem_numero, cep) 
-values (12, 'São Gonçalo', 'Nova Cidade', 'Rio de Janeiro', 'Rua Aquidabã', null, null, 0, '24455450');
+values (12, 'Nome da cidade', 'Nome do bairro', 'Rio de Janeiro', 'Descrição do endereço', null, null, 0, '24455000');
 
 insert into login (id_usuario, matricula, senha) values (1, 'us0001', '123');
 insert into login (id_usuario, matricula, senha) values (2, 'us0002', '123');
