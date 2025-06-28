2cf0790548c105b330fcabeba7981884be057e13
Arthur Martins <arthurmartins@mac.home>
2025-06-28 21:21:52 +0100

    Testes de integração para o PerfilController

---
+++ src/test/java/br/com/fiapfood/integracao/controllers/PerfilControllerIT.java
@@ -0,0 +1,52 @@
+package br.com.fiapfood.integracao.controllers;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/db_clean.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = {"/db_load.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class PerfilControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Nested
+    class BuscarTodosRequest {
+
+        @Test
+        void deveRetornarListaComPerfisCadastrados() throws Exception {
+            // Arrange
+
+            // Act & Assert
+            given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when()
+                    .get("/perfis")
+                    .then()
+                    .log().all()
+                    .statusCode(HttpStatus.OK.value())
+                    .body("$", hasSize(2))
+                    .body("[0]", hasEntry("id", 1))
+                    .body("[0]", hasEntry("nome", "Dono"))
+                    .body("[1]", hasEntry("id", 2))
+                    .body("[1]", hasEntry("nome", "Cliente"));
+        }
+    }
+}
