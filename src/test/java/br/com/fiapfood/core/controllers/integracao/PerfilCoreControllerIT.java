097addef417b4dbc112d819d67c2361273f6f647
arthurmezaonik <arthur.mezaonik@gmail.com>
2025-07-27 11:35:46 +0100

    Teste de integração no PerfilCoreController

--- src/test/java/br/com/fiapfood/core/controllers/integracao/PerfilCoreControllerIT.java
+++ src/test/java/br/com/fiapfood/core/controllers/integracao/PerfilCoreControllerIT.java
@@ -1,59 +1,63 @@
-package br.com.fiapfood.core.controllers.unitarios;
+package br.com.fiapfood.core.controllers.integracao;
 
 import br.com.fiapfood.core.controllers.impl.PerfilCoreController;
 import br.com.fiapfood.core.controllers.interfaces.IPerfilCoreController;
-import br.com.fiapfood.core.entities.dto.perfil.PerfilCoreDto;
 import br.com.fiapfood.core.exceptions.ExclusaoPerfilNaoPermitidaException;
 import br.com.fiapfood.core.exceptions.NomePerfilDuplicadoException;
 import br.com.fiapfood.core.exceptions.PerfilInvalidoException;
 import br.com.fiapfood.core.exceptions.PerfilNaoEncontradoException;
+import br.com.fiapfood.core.gateways.impl.PerfilGateway;
+import br.com.fiapfood.core.gateways.impl.UsuarioGateway;
+import br.com.fiapfood.core.gateways.interfaces.IPerfilGateway;
+import br.com.fiapfood.core.gateways.interfaces.IUsuarioGateway;
+import br.com.fiapfood.core.usecases.perfil.impl.*;
 import br.com.fiapfood.core.usecases.perfil.interfaces.*;
-import org.junit.jupiter.api.*;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import br.com.fiapfood.infraestructure.repositories.interfaces.IPerfilRepository;
+import br.com.fiapfood.infraestructure.repositories.interfaces.IUsuarioRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+@SpringBootTest
+@Sql(scripts = {"/db_clean.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = {"/db_load.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class PerfilCoreControllerIT {
 
-public class PerfilCoreControllerTest {
-
-    @Mock
-    private IBuscarTodosPerfisUseCase buscarTodosUseCase;
-    @Mock
-    private IBuscarPerfilPorIdUseCase buscarPorIdUseCase;
-    @Mock
-    private ICadastrarPerfilUseCase cadastrarPerfilUseCase;
-    @Mock
-    private IAtualizarNomePerfilUseCase atualizarNomePerfilUseCase;
-    @Mock
-    private IInativarPerfilUseCase inativarPerfilUseCase;
-    @Mock
-    private IReativarPerfilUseCase reativarPerfilUseCase;
     private IPerfilCoreController perfilCoreController;
-
-    AutoCloseable mock;
+    @Autowired
+    IPerfilRepository perfilRepository;
+    @Autowired
+    IUsuarioRepository usuarioRepository;
 
     @BeforeEach
-    void setUp() {
-        mock = MockitoAnnotations.openMocks(this);
+    public void setup() {
+        IPerfilGateway perfilGateway = new PerfilGateway(perfilRepository);
+        IUsuarioGateway usuarioGateway = new UsuarioGateway(usuarioRepository);
+        ICadastrarPerfilUseCase cadastrarPerfilUseCase = new CadastrarPerfilUseCase(perfilGateway);
+        IAtualizarNomePerfilUseCase atualizarNomePerfilUseCase = new AtualizarNomePerfilUseCase(perfilGateway);
+        IInativarPerfilUseCase inativarPerfilUseCase = new InativarPerfilUseCase(perfilGateway, usuarioGateway);
+        IReativarPerfilUseCase reativarPerfilUseCase = new ReativarPerfilUseCase(perfilGateway);
+        IBuscarTodosPerfisUseCase buscarTodosPerfisUseCase = new BuscarTodosPerfisUseCase(perfilGateway);
+        IBuscarPerfilPorIdUseCase buscarPerfilPorIdUseCase = new BuscarPerfilPorIdUseCase(perfilGateway);
+
         perfilCoreController = new PerfilCoreController(
-                buscarTodosUseCase,
-                buscarPorIdUseCase,
+                buscarTodosPerfisUseCase,
+                buscarPerfilPorIdUseCase,
                 cadastrarPerfilUseCase,
                 atualizarNomePerfilUseCase,
                 inativarPerfilUseCase,
                 reativarPerfilUseCase
         );
-    }
-
-    @AfterEach
-    void tearDown() throws Exception {
-        mock.close();
     }
 
     @Nested
@@ -62,39 +66,27 @@ public class PerfilCoreControllerTest {
         @Test
         void deveBuscarTodosOsPerfisCadastradosChamandoUseCase() {
             // Arrange
-            List<PerfilCoreDto> perfilCoreDtoRetornadoDoUseCase = List.of(
-                    new PerfilCoreDto(1, "Admin", null, null),
-                    new PerfilCoreDto(2, "Cliente", null, null)
-            );
-
-            when(buscarTodosUseCase.buscar()).thenReturn(perfilCoreDtoRetornadoDoUseCase);
+            List<String> expectedNomes = List.of("Dono", "Cliente", "Administrador", "Entregador");
 
             // Act
             var perfisRetornadosDoController = perfilCoreController.buscarTodos();
 
             // Assert
-            assertThat(perfisRetornadosDoController.size()).isEqualTo(perfilCoreDtoRetornadoDoUseCase.size());
-            assertThat(perfisRetornadosDoController.getFirst().id()).isEqualTo(perfilCoreDtoRetornadoDoUseCase.getFirst().id());
-            assertThat(perfisRetornadosDoController.getFirst().nome()).isEqualTo(perfilCoreDtoRetornadoDoUseCase.getFirst().nome());
-            assertThat(perfisRetornadosDoController.getLast().id()).isEqualTo(perfilCoreDtoRetornadoDoUseCase.getLast().id());
-            assertThat(perfisRetornadosDoController.getLast().nome()).isEqualTo(perfilCoreDtoRetornadoDoUseCase.getLast().nome());
-
-            verify(buscarTodosUseCase, times(1)).buscar();
+            for (int i = 0; i < expectedNomes.size(); i++) {
+                assertThat(perfisRetornadosDoController.get(i).nome()).isEqualTo(expectedNomes.get(i));
+            }
         }
 
         @DisplayName("Buscar todos perfis cadastrados chamando o use case com erro. Nenhum perfil for encontrado")
         @Test
+        @Sql(scripts = "/db_clean.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD) // Limpando o BD para retornar o erro
         void deveLancarExcecaoSeNenhumPerfilEncontrado() {
             // Arrange
-            when(buscarTodosUseCase.buscar())
-                    .thenThrow(new PerfilNaoEncontradoException("Não foi encontrado nenhum perfil na base de dados."));
 
             // Act & Assert
             assertThatThrownBy(() -> perfilCoreController.buscarTodos())
                     .isInstanceOf(PerfilNaoEncontradoException.class)
                     .hasMessage("Não foi encontrado nenhum perfil na base de dados.");
-
-            verify(buscarTodosUseCase, times(1)).buscar();
         }
     }
 
@@ -105,39 +97,25 @@ public class PerfilCoreControllerTest {
         void deveBuscarPerfilPorIdChamandoUseCase() {
             // Arrange
             int id = 1;
-            PerfilCoreDto perfilCoreDtoRetornadoDoUseCase = new PerfilCoreDto(
-                    1,
-                    "Admin",
-                    null,
-                    null
-            );
-
-            when(buscarPorIdUseCase.buscar(id)).thenReturn(perfilCoreDtoRetornadoDoUseCase);
 
             // Act
             var  perfilRetornadosDoController = perfilCoreController.buscarPorId(id);
 
             // Assert
-            assertThat(perfilRetornadosDoController.id()).isEqualTo(perfilCoreDtoRetornadoDoUseCase.id());
-            assertThat(perfilRetornadosDoController.nome()).isEqualTo(perfilCoreDtoRetornadoDoUseCase.nome());
-
-            verify(buscarPorIdUseCase, times(1)).buscar(id);
+            assertThat(perfilRetornadosDoController.id()).isEqualTo(1);
+            assertThat(perfilRetornadosDoController.nome()).isEqualTo("Dono");
         }
 
         @DisplayName("Deve buscar perfil por id chamando o use case com erro. Perfil for encontrado")
         @Test
         void deveLancarExcecaoSeNenhumPerfilEncontradoAtravesDoId() {
             // Arrange
-            int id = 1;
-            when(buscarPorIdUseCase.buscar(id))
-                    .thenThrow(new PerfilInvalidoException("Não foi encontrado nenhum perfil com o id informado."));
+            int id = 100;
 
             // Act & Assert
             assertThatThrownBy(() -> perfilCoreController.buscarPorId(id))
                     .isInstanceOf(PerfilInvalidoException.class)
                     .hasMessage("Não foi encontrado nenhum perfil com o id informado.");
-
-            verify(buscarPorIdUseCase, times(1)).buscar(id);
         }
     }
 
@@ -148,30 +126,29 @@ public class PerfilCoreControllerTest {
         void deveCadastrarPerfilChamandoUseCase() {
             // Arrange
             String nomePerfil = "Funcionário";
-            doNothing().when(cadastrarPerfilUseCase).cadastrar(nomePerfil);
-            ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+            int sizeAntesDeAdicionarNovoPerfil = perfilCoreController.buscarTodos().size();
 
             // Act
             perfilCoreController.cadastrar(nomePerfil);
+            var todosPerfisAtualizado = perfilCoreController.buscarTodos(); // Usado para checar se foi mesmo cadastrado
+            boolean perfilCriado = todosPerfisAtualizado.stream()
+                    .anyMatch(perfil -> perfil.nome().equals(nomePerfil));
 
             // Assert
-            verify(cadastrarPerfilUseCase, times(1)).cadastrar(captor.capture());
-            assertThat(captor.getValue()).isEqualTo(nomePerfil);
+            assertThat(todosPerfisAtualizado.size()).isEqualTo(sizeAntesDeAdicionarNovoPerfil + 1);
+            assertThat(perfilCriado).isTrue();
         }
 
         @DisplayName("Deve cadastrar perfil chamando use case com erro. Nome ja cadastrado")
         @Test
         void deveLancarExcecaoSeNomeJaEstiverCadastrado() {
             // Arrange
-            String nomePerfil = "Funcionário";
-            doThrow(new NomePerfilDuplicadoException("Já existe um perfil com o nome informado.")).when(cadastrarPerfilUseCase).cadastrar(nomePerfil);
+            String nomePerfil = "Entregador";
 
             // Act & Assert
             assertThatThrownBy(() -> perfilCoreController.cadastrar(nomePerfil))
                     .isInstanceOf(NomePerfilDuplicadoException.class)
                     .hasMessage("Já existe um perfil com o nome informado.");
-
-            verify(cadastrarPerfilUseCase, times(1)).cadastrar(nomePerfil);
         }
     }
 
@@ -181,54 +158,41 @@ public class PerfilCoreControllerTest {
         @Test
         void deveAtualizarNomePerfilChamandoUseCase() {
             // Arrange
-            int perfilId = 1;
+            int perfilId = 4;
             String novoNome = "Funcionario";
-
-            doNothing().when(atualizarNomePerfilUseCase).atualizar(perfilId, novoNome);
-            ArgumentCaptor<String> nomeCaptor = ArgumentCaptor.forClass(String.class);
-            ArgumentCaptor<Integer> idCaptor = ArgumentCaptor.forClass(Integer.class);
 
             // Act
             perfilCoreController.atualizarNome(perfilId, novoNome);
+            var perfilAposAtualizacao = perfilCoreController.buscarPorId(perfilId);
 
             // Assert
-            verify(atualizarNomePerfilUseCase, times(1)).atualizar(idCaptor.capture(), nomeCaptor.capture());
-            assertThat(nomeCaptor.getValue()).isEqualTo(novoNome);
-            assertThat(idCaptor.getValue()).isEqualTo(perfilId);
+            assertThat(perfilAposAtualizacao.nome()).isEqualTo(novoNome);
         }
 
         @DisplayName("Deve atualizar o nome do perfil chamando a use case com erro. Novo nome ja cadastrado")
         @Test
         void deveLancarExcecaoSeNovoNomeJaCadastrado() {
             // Arrange
-            int perfilId = 1;
-            String novoNome = "Funcionario";
-
-            doThrow(new NomePerfilDuplicadoException("Já existe um perfil com o nome informado.")).when(atualizarNomePerfilUseCase).atualizar(perfilId,novoNome);
+            int perfilId = 4;
+            String novoNome = "Administrador";
 
             // Act & Assert
             assertThatThrownBy(() -> perfilCoreController.atualizarNome(perfilId, novoNome))
                     .isInstanceOf(NomePerfilDuplicadoException.class)
                     .hasMessage("Já existe um perfil com o nome informado.");
-
-            verify(atualizarNomePerfilUseCase, times(1)).atualizar(perfilId, novoNome);
         }
 
         @DisplayName("Deve atualizar o nome do perfil chamando a use case com erro. Perfil nao encontrado através do id.")
         @Test
         void deveLancarExcecaoSePerfilNaoEncontradoPorId() {
             // Arrange
-            int perfilId = 1;
+            int perfilId = 100;
             String novoNome = "Funcionario";
-
-            doThrow(new PerfilInvalidoException("Não foi encontrado nenhum perfil com o id informado.")).when(atualizarNomePerfilUseCase).atualizar(perfilId,novoNome);
 
             // Act & Assert
             assertThatThrownBy(() -> perfilCoreController.atualizarNome(perfilId, novoNome))
                     .isInstanceOf(PerfilInvalidoException.class)
                     .hasMessage("Não foi encontrado nenhum perfil com o id informado.");
-
-            verify(atualizarNomePerfilUseCase, times(1)).atualizar(perfilId, novoNome);
         }
     }
 
@@ -238,33 +202,26 @@ public class PerfilCoreControllerTest {
         @Test
         void deveInativarPerfilComSucessoChamandoUseCase() {
             // Arrange
-            int perfilId = 1;
-
-            doNothing().when(inativarPerfilUseCase).inativar(perfilId);
-            ArgumentCaptor<Integer> idCaptor = ArgumentCaptor.forClass(Integer.class);
+            int perfilId = 4;
 
             // Act
             perfilCoreController.inativar(perfilId);
+            var perfilAposInativacao = perfilCoreController.buscarPorId(perfilId);
 
             // Assert
-            verify(inativarPerfilUseCase).inativar(idCaptor.capture());
-            assertThat(idCaptor.getValue()).isEqualTo(perfilId);
+            assertThat(perfilAposInativacao.dataInativacao()).isNotNull();
         }
 
         @DisplayName("Deve inativar perfil chamando a use case com erro. Perfil nao encontrado através do id.")
         @Test
         void deveLancarExcecaoSePerfilNaoEncontradoPorId() {
             // Arrange
-            int perfilId = 1;
-
-            doThrow(new PerfilInvalidoException("Não foi encontrado nenhum perfil com o id informado.")).when(inativarPerfilUseCase).inativar(perfilId);
+            int perfilId = 100;
 
             // Act & Assert
             assertThatThrownBy(() -> perfilCoreController.inativar(perfilId))
                     .isInstanceOf(PerfilInvalidoException.class)
                     .hasMessage("Não foi encontrado nenhum perfil com o id informado.");
-
-            verify(inativarPerfilUseCase, times(1)).inativar(perfilId);
         }
 
         @DisplayName("Deve inativar perfil chamando a use case com erro. Usuarios ativos associados com perfil.")
@@ -273,14 +230,10 @@ public class PerfilCoreControllerTest {
             // Arrange
             int perfilId = 1;
 
-            doThrow(new ExclusaoPerfilNaoPermitidaException("Não é possível inativar o perfil pois há usuário ativo associado ao perfil.")).when(inativarPerfilUseCase).inativar(perfilId);
-
             // Act & Assert
-            assertThatThrownBy(() -> perfilCoreController.inativar(perfilId))
+            Assertions.assertThatThrownBy(() -> perfilCoreController.inativar(perfilId))
                     .isInstanceOf(ExclusaoPerfilNaoPermitidaException.class)
                     .hasMessage("Não é possível inativar o perfil pois há usuário ativo associado ao perfil.");
-
-            verify(inativarPerfilUseCase, times(1)).inativar(perfilId);
         }
     }
 
@@ -290,33 +243,26 @@ public class PerfilCoreControllerTest {
         @Test
         void deveReativarPerfilComSucessoChamandoUseCase() {
             // Arrange
-            int perfilId = 1;
-
-            doNothing().when(reativarPerfilUseCase).reativar(perfilId);
-            ArgumentCaptor<Integer> idCaptor = ArgumentCaptor.forClass(Integer.class);
+            int perfilId = 3;
 
             // Act
             perfilCoreController.reativar(perfilId);
+            var perfilAposReativacao = perfilCoreController.buscarPorId(perfilId);
 
             // Assert
-            verify(reativarPerfilUseCase).reativar(idCaptor.capture());
-            assertThat(idCaptor.getValue()).isEqualTo(perfilId);
+            assertThat(perfilAposReativacao.dataInativacao()).isNull();
         }
 
         @DisplayName("Deve reativar perfil chamando a use case com erro. Perfil nao encontrado através do id.")
         @Test
         void deveLancarExcecaoSePerfilNaoEncontradoPorId() {
             // Arrange
-            int perfilId = 1;
-
-            doThrow(new PerfilInvalidoException("Não foi encontrado nenhum perfil com o id informado.")).when(reativarPerfilUseCase).reativar(perfilId);
+            int perfilId = 100;
 
             // Act & Assert
             assertThatThrownBy(() -> perfilCoreController.reativar(perfilId))
                     .isInstanceOf(PerfilInvalidoException.class)
                     .hasMessage("Não foi encontrado nenhum perfil com o id informado.");
-
-            verify(reativarPerfilUseCase, times(1)).reativar(perfilId);
         }
     }
 }
