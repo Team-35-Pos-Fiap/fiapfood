b48b5d15796db22efc57d3d354ffe72acd1465d6
tmsouzarj <tmsouzarj@gmail.com>
2025-07-31 00:55:14 -0300

    - alterando os scripts de para o banco mysql.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,70 +1,110 @@
-CREATE TABLE IF NOT EXISTS `cardapio` (
-                            `disponivel_apenas_restaurante` tinyint(1) NOT NULL,
-                            `preco` double NOT NULL,
-                            `id` binary(16) NOT NULL,
-                            `descricao` varchar(255) NOT NULL,
-                            `foto_prato` varchar(255) NOT NULL,
-                            `nome` varchar(255) NOT NULL,
-                            PRIMARY KEY (`id`)
+set foreign_key_checks = 0;
+
+drop table if exists endereco;
+drop table if exists restaurante;
+drop table if exists login;
+drop table if exists usuario;
+drop table if exists tipo_culinaria;
+drop table if exists atendimento;
+drop table if exists item;
+drop table if exists imagem;
+drop table if exists perfil;
+
+set foreign_key_checks = 1;
+
+create table perfil (
+	id int not null auto_increment,
+	nome varchar(50) not null,
+	data_criacao date not null,
+	data_inativacao date null,
+	primary key(id)
 );
 
-CREATE TABLE IF NOT EXISTS `endereco` (
-                            `numero` int DEFAULT NULL,
-                            `sem_numero` tinyint DEFAULT NULL,
-                            `id` binary(16) NOT NULL,
-                            `bairro` varchar(255) DEFAULT NULL,
-                            `cep` varchar(255) DEFAULT NULL,
-                            `cidade` varchar(255) DEFAULT NULL,
-                            `complemento` varchar(255) DEFAULT NULL,
-                            `endereco` varchar(255) DEFAULT NULL,
-                            `estado` varchar(255) DEFAULT NULL,
-                            PRIMARY KEY (`id`)
+create table endereco (
+    id binary(16) not null,
+    cidade varchar(100) not null,
+    bairro varchar(100) not null,
+    estado varchar(100) not null,
+    endereco varchar(150) not null,
+    numero int null,
+    complemento varchar(150) null,
+    cep varchar(15) not null,
+    primary key(id)
 );
 
-CREATE TABLE IF NOT EXISTS `login` (
-                         `id` binary(16) NOT NULL,
-                         `matricula` varchar(255) DEFAULT NULL,
-                         `senha` varchar(255) DEFAULT NULL,
-                         PRIMARY KEY (`id`),
-                         UNIQUE KEY `UK_matricula` (`matricula`)
+create table login (
+    id binary(16) not null,
+    matricula varchar(6) not null,
+    senha varchar(10) not null,
+    primary key(id)
 );
 
-CREATE TABLE IF NOT EXISTS `perfil` (
-                          `id` int NOT NULL AUTO_INCREMENT,
-                          `nome` varchar(255) DEFAULT NULL,
-                          PRIMARY KEY (`id`)
+create table usuario (
+    id binary(16) not null,
+	nome varchar(150) not null,
+	email varchar(70) not null,
+	ativo tinyint(1) not null default 1,
+	data_criacao datetime not null,
+	data_atualizacao datetime null,
+	id_perfil int not null,
+    id_endereco binary(16) not null,
+    id_login binary(16) not null,
+	primary key(id),
+    unique key UK_endereco (id_endereco),
+    unique key UK_login (id_login),
+    unique key UK_email (email),
+    constraint FK_perfil foreign key (id_perfil) references perfil (id),
+    constraint FK_login foreign key (id_login) references login (id),
+    constraint FK_endereco_usuario foreign key (id_endereco) references endereco (id)
 );
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-                           `ativo` tinyint DEFAULT NULL,
-                           `id_perfil` int DEFAULT NULL,
-                           `data_atualizacao` datetime(6) DEFAULT NULL,
-                           `data_criacao` datetime(6) NOT NULL,
-                           `id` binary(16) NOT NULL,
-                           `id_endereco` binary(16) DEFAULT NULL,
-                           `id_login` binary(16) DEFAULT NULL,
-                           `email` varchar(255) DEFAULT NULL,
-                           `nome` varchar(255) DEFAULT NULL,
-                           PRIMARY KEY (`id`),
-                           UNIQUE KEY `UK_endereco` (`id_endereco`),
-                           UNIQUE KEY `UK_login` (`id_login`),
-                           UNIQUE KEY `UK_email` (`email`),
-                           KEY `FK_perfil` (`id_perfil`),
-                           CONSTRAINT `FK_perfil` FOREIGN KEY (`id_perfil`) REFERENCES `perfil` (`id`),
-                           CONSTRAINT `FK_login` FOREIGN KEY (`id_login`) REFERENCES `login` (`id`),
-                           CONSTRAINT `FK_endereco` FOREIGN KEY (`id_endereco`) REFERENCES `endereco` (`id`)
+create table tipo_culinaria (
+	id int not null auto_increment,
+	nome varchar(50) not null,
+	primary key(id)
 );
 
-CREATE TABLE IF NOT EXISTS `restaurante` (
-                                             `horario_funcionamento` datetime(6) NOT NULL,
-    `id` binary(16) NOT NULL,
-    `id_dono_restaurante` binary(16) DEFAULT NULL,
-    `id_endereco` binary(16) DEFAULT NULL,
-    `nome` varchar(255) NOT NULL,
-    `tipo_cozinha` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `FK_usuario_restaurante` (`id_dono_restaurante`),
-    KEY `FK_endereco_restaurante` (`id_endereco`),
-    CONSTRAINT `FK_endereco_restaurante` FOREIGN KEY (`id_endereco`) REFERENCES `endereco` (`id`),
-    CONSTRAINT `FK_usuario_restaurante` FOREIGN KEY (`id_dono_restaurante`) REFERENCES `usuario` (`id`)
-    );
+create table restaurante (
+    id binary(16) not null,
+    nome varchar(255) not null,
+	ativo tinyint(1) not null default 1,
+    id_endereco binary(16) not null,
+    id_usuario binary(16) not null,
+    id_tipo_culinaria int not null,
+    primary key (id),
+	constraint FK_endereco_restaurante  foreign key(id_endereco) references endereco(id),
+    constraint FK_usuario foreign key(id_usuario) references usuario(id),
+    constraint FK_tipo_culinaria foreign key(id_tipo_culinaria) references tipo_culinaria(id)
+);
+
+create table atendimento (
+	id_atendimento binary(16) not null,
+	dia enum('SEGUNDA_FEIRA', 'TERÇA_FEIRA', 'QUARTA_FEIRA', 'QUINTA_FEIRA', 'SEXTA_FEIRA', 'SÁBADO', 'DOMINGO'),
+	inicio_atendimento time not null,
+	termino_atendimento time not null,
+	id_restaurante binary(16) not null,	  
+	primary key (id_atendimento),
+	constraint FK_atendimento_restaurante foreign key(id_restaurante) references restaurante(id)
+);
+
+create table imagem (
+	id binary(16) not null,
+	nome varchar(50) not null,
+	tipo varchar(20) not null,
+	conteudo blob not null,
+	primary key(id)
+);
+
+create table item (
+	id binary(16) not null,
+	nome varchar(255) not null,
+    descricao varchar(255) null,
+ 	preco decimal(9, 2) not null,
+ 	disponivel_consumo_presencial tinyint(1) not null default 0,
+	disponivel tinyint(1) not null default 1,
+	id_imagem binary(16) not null,
+	id_restaurante binary(16) not null,	  
+	primary key (id),
+    constraint FK_imagem foreign key(id_imagem) references imagem(id),
+	constraint FK_atendimento_item foreign key(id_restaurante) references restaurante(id)
+);
